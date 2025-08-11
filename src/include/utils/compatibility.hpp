0e65236f7bf8cf2bdc81def71edfb51144a1171a
Yves <yves@motherduck.com>
2025-08-11 16:54:18 -0700

    Update compatibility.hpp

--- src/include/utils/compatibility.hpp
+++ src/include/utils/compatibility.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 // Version comparison macros
-#define DUCKDB_VERSION_AT_MOST(major, minor, patch) \
-    (DUCKDB_MAJOR_VERSION < (major) || \
-     (DUCKDB_MAJOR_VERSION == (major) && DUCKDB_MINOR_VERSION <= (minor)) || \
-     (DUCKDB_MAJOR_VERSION == (major) && DUCKDB_MINOR_VERSION == (minor) && DUCKDB_PATCH_VERSION <= (patch)))
+#define DUCKDB_VERSION_AT_MOST(major, minor, patch)                                                                    \
+	(DUCKDB_MAJOR_VERSION < (major) || (DUCKDB_MAJOR_VERSION == (major) && DUCKDB_MINOR_VERSION <= (minor)) ||         \
+	 (DUCKDB_MAJOR_VERSION == (major) && DUCKDB_MINOR_VERSION == (minor) && DUCKDB_PATCH_VERSION <= (patch)))
